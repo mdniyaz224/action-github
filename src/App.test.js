0f40d8840a94d301a4c9e9c1417747afc7faae71
Niyaz <mdniyaz1842000@gmail.com>
2026-09-26 17:15:32 +0530

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByRole('heading', { name: /action github/i });
+  expect(heading).toBeInTheDocument();
+});
+
+test('renders welcome message', () => {
+  render(<App />);
+  const message = screen.getByText(/welcome to the ci demo app/i);
+  expect(message).toBeInTheDocument();
 });
